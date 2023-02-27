824728da33cec8856ccc883b4f759aa5f7f2848f
NdangoM <cynthiamufamadi@gmail.com>
2023-02-27 14:26:30 -0800

    the last digit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
+#include < stdio.h>
 /**
- * main - entry point
+ * main - Entry point
+ *
+ * Description: 'the program's description'
  *
  * return: always 0
  */
