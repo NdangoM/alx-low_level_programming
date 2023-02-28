4703412eee582c5b1405da8de279b7431c46d384
NdangoM <cynthiamufamadi@gmail.com>
2023-02-28 06:16:05 -0800

    reset using pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - update value of var using pointer
+ * @n: int to updat
+ * Return: Always 0
+ */
+void reset_to_98(int *n);
+{
+	*n = 98;
+	return (0);
+}
