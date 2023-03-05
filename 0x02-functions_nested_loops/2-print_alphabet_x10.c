c373f56201fc6fbd828179596edd9d7dbe344bef
NdangoM <cynthiamufamadi@gmail.com>
2023-03-05 12:23:10 -0800

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,10 +10,11 @@ void print_alphabet_x10(void)
 
 	i = 0;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (letter = 'a'; (letter <= 'z'); letter++)
 		_putchar(letter);
+
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
