ccaad735643f55ac7fedd11e7744e6ddd9f2b261
NdangoM <cynthiamufamadi@gmail.com>
2023-03-06 09:18:24 -0800

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - chescks for alphabetic character
+ * @c: is the char to be checked
+ * Return: 1 is a letter, lowercase or uppercase or 0
+ */
+int _isalpha(int c)
+{
+	if ((c <= 'A' && c >= 'Z') || (c <= 'a' && c >= 'z'))
+		return (1);
+	else
+		return (0);
+}
