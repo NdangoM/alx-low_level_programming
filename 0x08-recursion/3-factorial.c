61f276ac3559682862c3301da42277b2022162c6
NdangoM <cynthiamufamadi@gmail.com>
2023-03-08 17:23:26 -0800

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,10 +9,9 @@
 int factorial(int n)
 {
 	if (n == 0)
-	{
-		if (n > 0)
-			return (n * factorial(n - 1));
-	}
+	return (1);
+	else if (n > 0)
+	return (n * factorial(n - 1));
 	else
-		return (-1);
+	return (-1);
 }
