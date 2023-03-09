744a7f46508722873fa3d63024b68319ba6c2d86
NdangoM <cynthiamufamadi@gmail.com>
2023-03-08 17:00:12 -0800

    string length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,13 +3,13 @@
 /**
  * _strlen_recursion - returns the length of a string
  * @s: input for the string
- * _strlen_recursion - return value
- */
+ * Return: length of string
+  */
 
 int _strlen_recursion(char *s)
 {
 	if (*s == 0)
 		return (0);
 
-	return (isprint(s[0]) + _strlen_recursion(s + 1));
+	return (s[0] + _strlen_recursion(s + 1));
 }
