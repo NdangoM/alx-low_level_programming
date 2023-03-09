85455140dfbc35b30ae018c96b43cd2143faad88
NdangoM <cynthiamufamadi@gmail.com>
2023-03-08 16:19:41 -0800

    initial commit

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: input for the string
+ * _strlen_recursion - return value
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (strlen(*s) == 0)
+		return (0);
+
+	return (isprint(s[0]) + _strlen_recursion(s + 1));
+}
