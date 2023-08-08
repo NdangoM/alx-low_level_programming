dd10dd05a3d206be98ecc2c393384a934b0cb4bd
NdangoM <cynthiamufamadi@gmail.com>
2023-08-08 01:02:22 -0700

    1-strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,14 +12,19 @@
 
 char *_strdup(char *str)
 {
-	long unsigned int i, len;
+	unsigned int i;
+	unsigned int len = 0;
 	char *t;
 
-	len = 6;
 	t = malloc((len + 1) * sizeof(str));
+
 	if (str == NULL || t == NULL)
 	{
 		return (NULL);
+	}
+	while (str[len] != '\0')
+	{
+		len++;
 	}
 	i = 0;
 	for (i = 0; i < len; i++)
