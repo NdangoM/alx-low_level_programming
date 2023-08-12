7aad0998baa36b5f5927fca38a12231b0c37e64a
NdangoM <cynthiamufamadi@gmail.com>
2023-08-12 06:35:38 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase alphabets
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int i = 'a';
+
+	for (i =  'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
